79de7b1fe1807fc1c94429c627eb25689612141c
Sam <sammy.unterman@gmail.com>
2016-06-06 22:10:50 -0400

    bug testing delete method round 2

--- contact.rb
+++ contact.rb
@@ -152,15 +152,15 @@ class Contact
 
     delete_var = delete_id.to_i #do i need to do this? who knows...
     @@contacts.each do |item| #iterate over each instance of contact in the master array
-        if contact_item.id == delete_var #compare user input id num with the id in each instance
+        if item.id == delete_var #compare user input id num with the id in each instance
           puts @@contacts.index
         end
 
 
 
-      var = @@contacts
-      puts @@contact(contact_delete)
-      @@contacts.delete_at(contact_delete)
+      # var = @@contacts
+      # puts @@contact(contact_delete)
+      # @@contacts.delete_at(contact_delete)
 
     end
   end
